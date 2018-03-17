dabf6864a3b71024cd066dc9186728a60a3c90b7
Ryan Adolf <ryanadolf123@gmail.com>
2018-03-17 01:44:46 -0700

    Notify user that application has started

--- src/onelog/Main.java
+++ src/onelog/Main.java
@@ -11,6 +11,7 @@ public class Main {
     private static Config config = ConfigurationParser.getConfiguration();
 
     public static void main(String[] args) {
+        System.out.println("In a hole in the ground there lived a hobbit.");
         UserNotificationApp.doLaunch();
         BackgroundProcess proc = new BackgroundProcess(config.logConfig.volume, (device) -> {
             new LogProcessor(device).go();
